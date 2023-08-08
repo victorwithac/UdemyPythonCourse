0a433b9234f4cbd99ba65a3d66d47139baa9243c
Victor Marques <victormarqueswork@gmail.com>
2023-08-08 07:15:39 -0300

    update

--- PythonProject/Section4_Python 3 intermediário (com programação procedural e funcional)/Section4_Aulas/Funcoes/escopo_func.py
+++ PythonProject/Section4_Python 3 intermediário (com programação procedural e funcional)/Section4_Aulas/Funcoes/escopo_func.py
@@ -1,25 +1,29 @@
-
-
-x = 1
+# x = 1#
+# def escopo():
+#     x = 10
+#     def outra_funcao():
+#         global x
+#         x = 11
+#         y = 2
+#         print(x, y)
+#     outra_funcao()
+#     print(x)
+#     x = 5#
+# print(x)
+# escopo()
+# print('*-*' * 15)
+# print(x)
 
 def escopo():
-
     x = 10
-
     def outra_funcao():
-        global x
-        x = 11
         y = 2
         print(x, y)
     outra_funcao()
 
-    print(x)
-    x = 5
-
-print(x)
 escopo()
-print('*-*' * 15)
-print(x)
+
+
 
 
 
--- PythonProject/Section4_Python 3 intermediário (com programação procedural e funcional)/Section4_Aulas/Funcoes/intro_a_func.py
+++ PythonProject/Section4_Python 3 intermediário (com programação procedural e funcional)/Section4_Aulas/Funcoes/intro_a_func.py
@@ -6,9 +6,49 @@
 
 #Usando os parametros dentro da função pode-se sempre mudar a função!!!!
 
-def saudacao(nome='Ninguém'):
-    print(f'Ola, {nome}!')
+# def saudacao(nome='Ninguém'):
+#     print(f'Ola, {nome}!')
+#
+# saudacao('Victor')
+# saudacao('Luiz')
+# saudacao()
 
-saudacao('Victor')
-saudacao('Luiz')
-saudacao()
+
+# def imprimir(fechadura):
+#     return (fechadura)
+# 
+# imprimir('chave')
+
+
+# def saudar(nome='Sem nome'):
+#     print(f'O python retornou meu argumento >>> {nome}')
+#
+# saudar('Victor')
+# saudar('Marques')
+# saudar('Dos')
+# saudar('Santos')
+# saudar()
+
+# def function(x='No colour'):
+#     print(f'The return of my function is, {x}')
+#
+# function('Blue')
+# function('Black')
+# function('White')
+# function()
+
+
+# def sum(x, y, z=0):
+#     print(f'{x=} {y=} {z=}', '|', 'x + y + z =', x + y + z)
+#
+# # sum(1422, 4542)
+# # sum(1, 2)
+# # sum(y=2, x=1) #argumentos nomeados, quando eu digo com o '='qual é o meu argumento
+#
+# sum(1, 2)
+
+def my_function(x, y, z):
+    resultado = x + y + z
+    print(resultado)
+
+my_function(1, 2, 3)
