355fc4f7053369c903c7e714e4783d5a45e3b0e9
Victor Marques <victormarqueswork@gmail.com>
2023-08-11 07:18:57 -0300

    update

--- PythonProject/Section4_Python 3 intermediário (com programação procedural e funcional)/Section4_Aulas/Dicionarios/intro_dicionarios.py
+++ PythonProject/Section4_Python 3 intermediário (com programação procedural e funcional)/Section4_Aulas/Dicionarios/intro_dicionarios.py
@@ -1,13 +1,32 @@
-pessoa = {
-    'nome': 'Victor',
-    'sobrenome': 'Marques',
-    'idade': 18,
-    'endereços': [
-        {'rua': 'tal tal', 'número': 123},
-        {'rua': 'outra rua', 'número': 321},
-    ]
+# pessoa = {
+#     'nome': 'Victor',
+#     'sobrenome': 'Marques',
+#     'idade': 18,
+#     'endereços': [
+#         {'rua': 'tal tal', 'número': 123},
+#         {'rua': 'outra rua', 'número': 321},
+#     ]
+#
+# }
+#
+# print(pessoa['endereços'])
+
+pessoas = {'alguma coisa':
+    [
+    {'nome': ['João', 'Maria', 'Marcos', 'Lucas'],},
+    {'sobrenome': ['Oliveira', 'Santos', 'Marques', 'Silva'],}
+]
 
 }
 
-print(pessoa['endereços'])
+print(pessoas['nome'][2])
+print(pessoas['sobrenome'][2])
 
+print(pessoas['alguma coisa']['nome'][1])
+print(pessoas['alguma coisa'][1]['sobrenome'][1])
+
+
+# dicionario > chave > lista > 2 dicionarios
+
+for chave in pessoas:
+    print(chave, pessoas[chave][1])
--- PythonProject/Section4_Python 3 intermediário (com programação procedural e funcional)/Section4_Exercicios/funcoes2.py
+++ PythonProject/Section4_Python 3 intermediário (com programação procedural e funcional)/Section4_Exercicios/funcoes2.py
@@ -11,18 +11,18 @@
 # print(triplicar(5))
 # print(quadriplicar(5))
 
-def operacao(multiplicar):
-    def operar(numero):
-        return numero * multiplicar
-    return operar
-
-duplicar = operacao(2)
-triplicar = operacao(3)
-quadruplicar = operacao(4)
-
-print(duplicar(10))
-print(triplicar(5))
-print(quadruplicar(20))
+# def operacao(multiplicar):
+#     def operar(numero):
+#         return numero * multiplicar
+#     return operar
+#
+# duplicar = operacao(2)
+# triplicar = operacao(3)
+# quadruplicar = operacao(4)
+#
+# print(duplicar(10))
+# print(triplicar(5))
+# print(quadruplicar(20))
 
 
 # numero = int(input('Digite um número: '))
@@ -36,3 +36,19 @@ print(quadruplicar(20))
 # print(duplicar(numero))
 # print(triplicar(numero))
 # print(quadruplicar(numero))
+
+
+def operacao(multiplicar):
+    def operar(numero):
+        return numero * multiplicar
+    return operar
+
+numero = int(input('Digite um número para operar: '))
+
+duplicar = operacao(2)
+triplicar = operacao(3)
+quadruplicar = operacao(4)
+
+print(duplicar(numero))
+print(triplicar(numero))
+print(quadruplicar(numero))
