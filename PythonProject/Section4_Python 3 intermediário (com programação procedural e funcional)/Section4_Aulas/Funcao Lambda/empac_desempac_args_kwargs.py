6f398547bb92e9a85b7bd06ec8e4c7a37782fe93
Victor Marques <victormarqueswork@gmail.com>
2023-08-21 21:51:38 -0300

    update

--- PythonProject/Section4_Python 3 intermediário (com programação procedural e funcional)/Section4_Aulas/Funcao Lambda/empac_desempac_args_kwargs.py
+++ PythonProject/Section4_Python 3 intermediário (com programação procedural e funcional)/Section4_Aulas/Funcao Lambda/empac_desempac_args_kwargs.py
@@ -26,4 +26,23 @@ dados_pessoa = {
 
 # print(pessoa, dados_pessoa)
 
-# pessoas_completa = {}
+pessoas_completa = {**pessoa,'chave': 5, **dados_pessoa, 'nova_chave': 4}
+# print(pessoas_completa)
+
+#args e kwargs
+
+def mostro_argumentos_nomeados(*args, **kwargs):
+    print(args)
+    print()
+    for chave, valor in kwargs.items():
+        print(chave, valor)
+
+# mostro_argumentos_nomeados(1, 2, x=2, nome='Joana', qlq=123, y=5)
+
+configuracoes = {
+    'arg1':1,
+    'args2':2,
+    'arg3':3,
+    'arg4':4,
+}
+mostro_argumentos_nomeados(**configuracoes)
