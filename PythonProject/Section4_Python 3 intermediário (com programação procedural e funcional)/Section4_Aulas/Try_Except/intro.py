9288786dabe359b8237a431e86abe1ec8155b3aa
Victor Marques <victormarqueswork@gmail.com>
2023-09-06 06:19:03 -0300

    update

--- PythonProject/Section4_Python 3 intermediário (com programação procedural e funcional)/Section4_Aulas/Try_Except/intro.py
+++ PythonProject/Section4_Python 3 intermediário (com programação procedural e funcional)/Section4_Aulas/Try_Except/intro.py
@@ -1,15 +1,16 @@
 #Try, except, else and finally
 
 
-
 try:
     a = 18
     # b = 0
     c = a / b
     print('Row 1')
     print('Row 2')
-except (ZeroDivisionError, NameError):
+except (ZeroDivisionError, NameError) as error:
     print('Can\'t divide by zero or Name is not defined')
+    print('MSG', error)
+    print('Nome:', error.__class__.__name__)
 # except NameError:
 #     print('Name is not defined')
 # except Exception:
