2ec61d40e910198759d3c1f5734390ea914fa04d
Victor Marques <victormarqueswork@gmail.com>
2023-09-19 21:45:22 -0300

    update

--- PythonProject/Section4_Python 3 intermediário (com programação procedural e funcional)/Section4_Aulas/JSON/module.py
+++ PythonProject/Section4_Python 3 intermediário (com programação procedural e funcional)/Section4_Aulas/JSON/module.py
@@ -14,7 +14,7 @@ person = [
         }
     },
     {
-        "name": 'Maria',
+        "name": 'Joana',
                 "lastname": 'Vieira',
     "age": 25,
     "ative": False,
@@ -31,20 +31,25 @@ BASE_DIR = os.path.dirname(__file__) #return only the path of the file
 SAVE_TO = os.path.join(BASE_DIR, 'file-python.json')
 
 
-print(BASE_DIR) #return only the path of the file
-print(__file__)#return the path and the name of the file
-print(FILE_NAME) #return only the name of the file
-print(SAVE_TO)
+# print(BASE_DIR) #return only the path of the file
+# print(__file__)#return the path and the name of the file
+# print(FILE_NAME) #return only the name of the file
+# print(SAVE_TO)
 
 
 with open(SAVE_TO, 'w+') as file:
     json.dump(person, file, indent=2)
+print(json.dumps(person, indent=2))
 
-# print(json.dumps(person, indent=2))
+print()
 
 with open(SAVE_TO, 'r') as file:
-    person = json.load(file)
-    print(person)
+    person_2 = json.load(file)
+    # print(person)
 
-    for persons in person:
-        print(persons['name'])
+
+    # for persons in person_2:
+    #     print(persons['name'])
+
+
+
