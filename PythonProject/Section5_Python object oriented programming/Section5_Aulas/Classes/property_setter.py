566b0a7f5ab35c479245cd95b0fd408537471540
Victor Marques <victormarqueswork@gmail.com>
2023-10-25 06:43:06 -0300

    update

--- PythonProject/Section5_Python object oriented programming/Section5_Aulas/Classes/property_setter.py
+++ PythonProject/Section5_Python object oriented programming/Section5_Aulas/Classes/property_setter.py
@@ -1,11 +1,34 @@
 class Pen:
     def __init__(self, color):
-        self.paint_color = color
-        self._color = self.paint_color #whenever there is an underline, it means it should not be modified
+        # self.paint_color = color
+        self.color = color
+        #whenever there is an underline, it means it should not be modified
 
 
-        @property
-        def color(self):
-            print('Property')
-            return self.paint_color
+    # @property
+    # def color(self):
+    #     print('Property')
+    #     return self.paint_color
+
+# def show_pen(pen):
+#     return pen.color
+
+    def get_color(self):
+        return self.color
+
+
+
+pen = Pen('Black')
+var = pen.get_color()
+print(var)
+
+# pen = Pen('Blue')
+
+# cap = Pen('Green')
+#
+# print(pen.color)
+# print(cap.color)
+# print(pen.color)
+
+# show_pen(pen)
 
